1bc5094cadbd6316824d649f2c358279ebc62008
henrymunro <henrymunro73@gmail.com>
2017-11-13 16:18:44 +0000

    Added missing PropTypes to the container component

--- app/views/js/organisms/QuestionArea/QuestionArea.js
+++ app/views/js/organisms/QuestionArea/QuestionArea.js
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from "prop-types";
 import { connect } from "react-redux";
 
 import * as selectors from "./reducer";
@@ -12,13 +13,7 @@ import Slider from "../../molecules/Slider";
 
 import styles from "./QuestionArea.css";
 
-@connect((store, ownProps) => {
-  return {
-    question: selectors.getQuestion(store),
-    allQuestionsCorrect: selectors.getAllQuestionsCorrect(store)
-  };
-}, Object.assign({}, actions))
-export default class QuestionsAreaOrganism extends React.Component {
+class QuestionsAreaOrganism extends React.Component {
   assembleSliders() {
     // Assembles the sliders from the list of questions pulled from the store
     const Sliders = this.props.question.options.map((option, key) => {
@@ -37,6 +32,7 @@ export default class QuestionsAreaOrganism extends React.Component {
   }
 
   render() {
+    console.log(this.props);
     return (
       <div className={styles.question_area_div}>
         <QuestionAreaBox correct={this.props.allQuestionsCorrect}>
@@ -51,4 +47,29 @@ export default class QuestionsAreaOrganism extends React.Component {
   }
 }
 
-QuestionsAreaOrganism.propTypes = {};
+QuestionsAreaOrganism.propTypes = {
+  question: PropTypes.shape({
+    question: PropTypes.string,
+    options: PropTypes.arrayOf(
+      PropTypes.shape({
+        option_0: PropTypes.string,
+        option_1: PropTypes.string,
+        currentOption: PropTypes.number,
+        correctOption: PropTypes.number
+      })
+    )
+  }),
+  allQuestionsCorrect: PropTypes.bool
+};
+
+@connect((store, ownProps) => {
+  return {
+    question: selectors.getQuestion(store),
+    allQuestionsCorrect: selectors.getAllQuestionsCorrect(store)
+  };
+}, Object.assign({}, actions))
+export default class QuestionsAreaOrganismContainer extends React.Component {
+  render() {
+    return <QuestionsAreaOrganism {...this.props} />;
+  }
+}
